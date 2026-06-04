99b59cc7480790c12c1e99efe0fa51d4907c347d
olgasuchinskaya <olgasuchinskaya2008@gmail.com>
2026-06-04 23:16:33 +0300

    добавила класс DietaryRecipe

--- recipe.py
+++ recipe.py
@@ -123,3 +123,26 @@ class ShoppingList:
         new = ShoppingList()
         new._items = self._items + other._items
         return new
+
+
+
+#1.4
+
+class DietaryRecipe(Recipe):
+    
+    def __init__(self, title, diet_type, ingredients=None):
+        if ingredients is None:
+            ingredients =[]
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    
+    def scale(self, ratio):
+        new = super().scale(ratio)
+        return DietaryRecipe(new.title, self.diet_type, new.ingredients)
+
+    
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
+        
+                
