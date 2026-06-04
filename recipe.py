9a29519334294763fa620686f8e0ede08b0b23b4
olgasuchinskaya <olgasuchinskaya2008@gmail.com>
2026-06-04 22:43:11 +0300

    добавила класс Recipe

--- recipe.py
+++ recipe.py
@@ -32,3 +32,44 @@ class Ingredient:
         return self.name == x.name and self.unit == x.unit
 
 
+#1.2
+
+class Recipe:
+    
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return ratio >0
+
+
+    def scale(self, ratio):
+        if self.is_valid_ratio(ratio):
+            a = []
+            for i in self.ingredients:
+                new_quantity = i.quantity * ratio
+                new = Ingredient(i.name, new_quantity, i.unit)
+                a.append(new)
+            return Recipe(self.title, a)
+        else:
+            raise ValueError("Коэффициент должен быть положительным")
+    
+    def __len__(self):
+        return len(self.ingredients)
+
+    
+    def __str__(self):
+        return f"{self.title}: {', '.join(str(i) for i in self.ingredients)}"
+
+
